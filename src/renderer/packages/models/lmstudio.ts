79f5dde3f20d20783d6e94b81ed411b2c26bb75c
tsja2001 <52786405+tsja2001@users.noreply.github.com>
2025-03-12 10:37:43 +0800

    first commit

---
+++ src/renderer/packages/models/lmstudio.ts
@@ -0,0 +1,79 @@
+import { Message } from 'src/shared/types'
+import Base, { onResultChange } from './base'
+import { ApiError } from './errors'
+
+// import LMStudio from 'LMStudio/browser'
+
+interface Options {
+    lmStudioHost: string
+    lmStudioModel: string
+    temperature: number
+
+}
+
+export default class LMStudio extends Base {
+    public name = 'LMStudio'
+
+    public options: Options
+    constructor(options: Options) {
+        super()
+        this.options = options
+    }
+
+    getHost(): string {
+        let host = this.options.lmStudioHost.trim()
+        if (host.endsWith('/')) {
+            host = host.slice(0, -1)
+        }
+        if (!host.startsWith('http')) {
+            host = 'http://' + host
+        }
+        if (host === 'http://localhost:1234') {
+            host = 'http://127.0.0.1:1234'
+        }
+        return host
+    }
+
+    async callChatCompletion(rawMessages: Message[], signal?: AbortSignal, onResultChange?: onResultChange): Promise<string> {
+        const messages = rawMessages.map(m => ({ role: m.role, content: m.content }))
+        const res = await this.post(
+            `${this.getHost()}/v1/chat/completions`,
+            { 'Content-Type': 'application/json' },
+            {
+                messages,
+                model: this.options.lmStudioModel,
+                temperature: this.options.temperature,
+                stream: true,
+                
+            },
+            signal,
+        )
+        let result = ''
+        await this.handleSSE(res, (message) => {
+            if (message === '[DONE]') {
+                return
+            }
+            const data = JSON.parse(message)
+            if (data.error) {
+                throw new ApiError(`Error from LMSdudio: ${JSON.stringify(data)}`)
+            }
+            const text = data.choices[0]?.delta?.content
+            if (text !== undefined) {
+                result += text
+                if (onResultChange) {
+                    onResultChange(result)
+                }
+            }
+        })
+        return result
+    }
+
+    async listModels(): Promise<string[]> {
+        const res = await this.get(`${this.getHost()}/v1/models`, {})
+        const json = await res.json()
+        if (! json['data']) {
+            throw new ApiError(JSON.stringify(json))
+        }
+        return json['data'].map((m: any) => m['id'])
+    }
+}
---
+++ src/shared/defaults.ts
@@ -0,0 +1,73 @@
+import { Theme, Config, Settings, ModelProvider, Session } from './types'
+import { v4 as uuidv4 } from 'uuid'
+
+export function settings(): Settings {
+    return {
+        aiProvider: ModelProvider.OpenAI,
+        openaiKey: '',
+        apiHost: 'https://api.openai.com',
+
+        azureApikey: '',
+        azureDeploymentName: '',
+        azureDalleDeploymentName: 'dall-e-3',
+        azureEndpoint: '',
+        chatglm6bUrl: '',
+        model: 'gpt-3.5-turbo',
+        temperature: 0.7,
+        topP: 1,
+        // openaiMaxTokens: 0,
+        // openaiMaxContextTokens: 4000,
+        openaiMaxContextMessageCount: 10,
+        // maxContextSize: "4000",
+        // maxTokens: "2048",
+
+        claudeApiKey: '',
+        claudeApiHost: 'https://api.anthropic.com',
+        claudeModel: 'claude-3-5-sonnet-20241022',
+
+        ollamaHost: 'http://127.0.0.1:11434',
+        ollamaModel: '',
+
+        lmStudioHost: 'http://127.0.0.1:1234',
+        lmStudioModel: '',
+
+        showWordCount: true,
+        showTokenCount: false,
+        showTokenUsed: true,
+        showModelName: true,
+        showMessageTimestamp: false,
+        userAvatarKey: '',
+        theme: Theme.FollowSystem,
+        language: 'en',
+        fontSize: 12,
+        spellCheck: true,
+
+        defaultPrompt: getDefaultPrompt(),
+
+        allowReportingAndTracking: true,
+
+        enableMarkdownRendering: true,
+
+        siliconCloudHost: 'https://api.siliconflow.cn',
+        siliconCloudKey: '',
+        siliconCloudModel: 'THUDM/glm-4-9b-chat',
+
+        ppioHost: 'https://api.ppinfra.com/v3/openai',
+        ppioKey: '',
+        ppioModel: 'deepseek/deepseek-r1/community',
+
+        autoGenerateTitle: true,
+    }
+}
+
+export function newConfigs(): Config {
+    return { uuid: uuidv4() }
+}
+
+export function getDefaultPrompt() {
+    return 'You are a helpful assistant. You can help me by answering my questions. You can also ask me questions.'
+}
+
+export function sessions(): Session[] {
+    return [{ id: uuidv4(), name: 'Untitled', messages: [], type: 'chat' }]
+}
